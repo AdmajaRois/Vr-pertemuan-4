a523d7e72cffa3e2ee9f65e18ddb0d8952c83dae
AdmajaRois <rois.da.silva@gmail.com>
2020-09-27 12:15:32 +0700

    uploading file

---
+++ Assets/Scripts/VRGaze.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VRGaze : MonoBehaviour
+{
+    public Image imgGaze;
+    public float totalTime = 2;
+    bool gvrStatus;
+    float gvrTimer;
+    public Text kubuText;
+
+    public int jarak;
+    private RaycastHit _hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gvrStatus){
+            gvrTimer += Time.deltaTime;
+            imgGaze.fillAmount = gvrTimer / totalTime;
+            print("Bagus");
+            kubuText.text = "Kubu merah";
+        }
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        if (Physics.Raycast(ray,out _hit, jarak))
+        {
+            if (_hit.collider.gameObject.GetComponent<Renderer>().material.color == Color.red)
+            {
+                kubuText.text = "Kubu merah";
+            }
+        }
+    }
+  
+
+    public void GVRon(){
+        gvrStatus = true;
+        print("GVR ON");
+    }
+
+    public void GVRoff(){
+        gvrStatus = false;
+        gvrTimer = 0;
+        imgGaze.fillAmount = 0;
+    }
+}
